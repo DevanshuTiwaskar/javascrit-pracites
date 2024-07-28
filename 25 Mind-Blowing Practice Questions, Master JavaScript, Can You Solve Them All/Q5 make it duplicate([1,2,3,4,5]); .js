2ec118099183e05b2cd67591e8eeb4c4d2abefbb
DEVANSHU TIWASKAR <devanshutiwaskar@gmail.com>
2024-07-28 16:48:17 +0530

    arry

--- 25 Mind-Blowing Practice Questions, Master JavaScript, Can You Solve Them All/Q5 make it duplicate([1,2,3,4,5]); .js
+++ 25 Mind-Blowing Practice Questions, Master JavaScript, Can You Solve Them All/Q5 make it duplicate([1,2,3,4,5]); .js
@@ -8,6 +8,15 @@
 //= [1,2,3,1,2,3];
 //------------------------------------------------------
 
+// The concat method in JavaScript is used to merge two or more arrays.
+//  This method does not change the existing arrays but instead 
+// returns a new array that contains the elements of the joined arrays.
+
+//------------------------------------------------------
+  
+// concat = joint
+
+
 // // ans ===
 
 // function duplicate(arr){
@@ -17,3 +26,35 @@
 // console.log(duplicate([1,2,3,4,5]))
 
 // // output == [1,2,3,4,5,1,2,3,4,5]
+
+
+
+
+// function duplicate(arr){
+//     return arr.concat(arr);
+// }
+
+// console.log(duplicate([1,2,3,4,5]));
+
+
+
+
+
+
+function duplicate(arr){
+    return arr.concat(arr);
+}
+
+console.log(duplicate([1,2,3,4,5]))
+
+
+
+
+
+
+
+
+
+
+
+
