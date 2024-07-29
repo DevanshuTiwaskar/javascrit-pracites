b256c9d0582b41c134c6551595ff061d523d4395
DEVANSHU TIWASKAR <devanshutiwaskar@gmail.com>
2024-07-29 17:52:21 +0530

    loop

---
+++ 25 Mind-Blowing Practice Questions, Master JavaScript, Can You Solve Them All/Q2 FUNCTION/Q8 write a javascript function that return a passed string with letter in alphabetical orde.js
@@ -0,0 +1,9 @@
+// Q8 write a javascript function that return a passed string with letter in alphabetical orde
+
+
+// function alphabeticalorder(str){
+//     return str.split("").sort().join("");
+// }
+
+// console.log(alphabeticalorder("devanshu"));
+// console.log(alphabeticalorder("tiwaskar"));
---
+++ 25 Mind-Blowing Practice Questions, Master JavaScript, Can You Solve Them All/Q2 FUNCTION/Q9 that accpet as string as a parameter and convert the first letter of eact word of the string in  upper case.js
@@ -0,0 +1,12 @@
+// Q9 write javascript function that accpet as string as a parameter and convert the first letter of eact word of the string in  upper case
+
+
+// function capitalizeKaro(str){
+//      let allword = str.split(" ").map(function(word){
+//        return word.charAt(0).toUpperCase() + word.substring(1)
+//      })
+//      return allword.join(" ");
+// }
+
+// console.log(capitalizeKaro("my name is devanshu"));
+// console.log(capitalizeKaro("teshan chalo"));
